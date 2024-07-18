a8312b34adeec9ad9d7df6d86cf17f307e431746
Mahdinkh7 <mahdinkh7@gmail.com>
2024-07-19 00:29:20 +0330

    first commit

---
+++ paint/paintApp.swift
@@ -0,0 +1,17 @@
+//
+//  paintApp.swift
+//  paint
+//
+//  Created by mahdi naghikhani on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct paintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
